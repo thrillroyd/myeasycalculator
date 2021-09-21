9a8d507d22016d4647e15a652b53c084f5a75041
thrillroyd <82989204+thrillroyd@users.noreply.github.com>
2021-09-21 08:27:01 +0700

    Just Once

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const chalk = require('chalk')
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function(req, res) {
+    var num1 = Number(req.body.num1)
+    var num2 = Number(req.body.num2)
+    var result = num1 + num2
+    res.send("Your Solution : " + result)
+
+})
+
+
+app.listen('3000', function() {
+    console.log(chalk.bold.green('Calculator is ready on port 3000'))
+})
